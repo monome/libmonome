8405fb25d0ccedbf8777f24068fab0fd3d4f97ce
William Light <will@illest.net>
2010-07-25 14:20:01 -0700

    simplify simple.py

--- examples/python/simple.py
+++ examples/python/simple.py
@@ -1,29 +1,24 @@
 import monome
 
+device = "osc.udp://127.0.0.1:8080/monome"
 
-class SimpleTest(monome.Monome):
-    def button_handler(self, event):
-        x, y = event.x, event.y
-        self.buttons[x][y] ^= 1
+def button_handler(event):
+    x, y = event.x, event.y
+    buttons[x][y] ^= 1
 
-        if self.buttons[x][event.y]:
-            self.led_on(x, y)
-        else:
-            self.led_off(x, y)
+    if buttons[x][event.y]:
+        m.led_on(x, y)
+    else:
+        m.led_off(x, y)
 
-    def run(self):
-        self.buttons = [[0 for x in xrange(0, 16)] for x in xrange(0, 16)]
-        self.register_handler(monome.BUTTON_DOWN, self.button_handler)
-        self.clear()
+buttons = [[0 for x in xrange(0, 16)] for x in xrange(0, 16)]
+m = monome.Monome(device, 8000)
+m.register_handler(monome.BUTTON_DOWN, button_handler)
 
-        print ("simple.py running, press some buttons!")
+print("simple.py running, press some buttons!")
 
-        try:
-            self.main_loop()
-        except:
-            self.clear()
-            self.close()
-
-if __name__ == "__main__":
-    app = SimpleTest("osc.udp://127.0.0.1:8080/monome", 8000)
-    app.run()
+try:
+	m.main_loop()
+except KeyboardInterrupt:
+	m.clear()
+	m.close()
