ac1c72127d8678e7a62b021d023a1124915f23a3
William Light <will@illest.net>
2010-07-24 21:47:53 -0700

    little python test change

--- bindings/python/test.py
+++ bindings/python/test.py
@@ -12,6 +12,7 @@ frame = """\
  . .. .
  .    .
  .    .
+ .    .
 """
 
 def frame_from_str(s):
@@ -20,4 +21,5 @@ def frame_from_str(s):
 
     return (row_from_str(x) for x in s.split("\n")[:8])
 
-m.led_frame(0, frame_from_str(frame))
+for i in xrange(0, 4):
+    m.led_frame(i, frame_from_str(frame))
