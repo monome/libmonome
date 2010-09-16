615275e8b017458d90a0783409e9f69e938cdd03
William Light <will@illest.net>
2010-09-16 00:58:40 -0700

    removed select() in monome_platform_write
    seems to fix a bug which would make a monome unresponsive to output LED messages after receiving a flood of them

---
+++ examples/torture.c
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2010 William Light <wrl@illest.net>
+ * 
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/**
+ * torture.c
+ * write a ton of data to a monome
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <monome.h>
+
+
+#define MONOME_OSC "osc.udp://127.0.0.1:8080/rove"
+#define MONOME_SERIAL "/dev/ttyUSB0"
+
+#define WIDTH  16
+#define HEIGHT 8
+
+
+typedef unsigned int uint_t;
+
+int main(int argc, char **argv) {
+	monome_t *monome;
+	uint_t w, h, x, y;
+
+	if( !(monome = monome_open(MONOME_OSC, "8000")) ) {
+		fprintf(stderr, "couldn't open monome\n");
+		exit(EXIT_FAILURE);
+	}
+
+	w = WIDTH;
+	h = HEIGHT;
+
+	for(;;)
+		for( x = 0; x < w; x++ )
+			for( y = 0; y < h; y++ )
+				monome_led(monome, x, y, random() & 1);
+}
