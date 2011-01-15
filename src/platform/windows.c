640810f00f16be495ab92ba1ce3c99d38ce4cd0f
William Light <wrl@illest.net>
2011-01-14 19:46:20 -0800

    add skeleton src/platform/windows.c

---
+++ src/platform/windows.c
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2010 William Light <wrl@illest.net>
+ * 
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <stdio.h>
+
+#include <monome.h>
+#include "internal.h"
+
+#warning "using the \"dummy\" platform, please make sure this is what you want!"
+
+monome_t *monome_platform_load_protocol(const char *proto) {
+	return NULL;
+}
+
+void monome_platform_free(monome_t *monome) {
+	return;
+}
+
+int monome_platform_open(monome_t *monome) {
+	printf("libmonome was compiled with the dummy platform and, hence, effectively does nothing.\n"
+		   "please recompile for any functionality!\n");
+	
+	return 1;
+}
+
+int monome_platform_close(monome_t *monome) {
+	return 1;
+}
+
+ssize_t monome_platform_write(monome_t *monome, const uint8_t *buf, ssize_t bufsize) {
+	return 1;
+}
+
+ssize_t monome_platform_read(monome_t *monome, uint8_t *buf, ssize_t count) {
+	return 1;
+}
+
+char *monome_platform_get_dev_serial(const char *path) {
+	return NULL;
+}
