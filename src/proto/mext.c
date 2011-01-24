7fd4e13df740e04faaa4f853035f2ee4e3444c73
William Light <wrl@illest.net>
2011-01-23 22:44:27 -0800

    implement mext_led()

--- src/proto/mext.c
+++ src/proto/mext.c
@@ -18,12 +18,62 @@
 
 #include <monome.h>
 #include "internal.h"
+#include "platform.h"
+#include "rotation.h"
 
 #include "mext.h"
 
-
 #define SELF_FROM(monome) mext_t *self = (mext_t *) monome
 
+/* message length excludes the one-byte header */
+static int msg_lengths[] = {
+	[CMD_LED_ON]        = 1,
+	[CMD_LED_OFF]       = 1,
+	[CMD_LED_ALL_ON]    = 0,
+	[CMD_LED_ALL_OFF]   = 0,
+	[CMD_LED_FRAME]     = 10,
+	[CMD_LED_ROW]       = 3,
+	[CMD_LED_COLUMN]    = 3,
+	[CMD_LED_INTENSITY] = 1
+};
+
+/* private */
+
+static int mext_write_msg(monome_t *monome, mext_msg_t *msg) {
+	monome_platform_write(monome, (uint8_t *) msg, msg_lengths[msg->header.cmd]);
+
+	return 0;
+}
+
+/* public */
+
+static int mext_led(monome_t *monome, uint_t x, uint_t y, uint_t on) {
+	mext_msg_t msg = {
+		.header = {
+			.addr = SS_LED_GRID,
+			.cmd  = !on
+		}
+	};
+
+	ROTATE_COORDS(monome, x, y);
+
+	msg.cmd.led.x = x;
+	msg.cmd.led.y = y;
+
+	return mext_write_msg(monome, &msg);
+}
+
+static int mext_open(monome_t *monome, const char *dev, const char *serial,
+                     const monome_devmap_t *m, va_list args) {
+	if( monome_platform_open(monome, dev) )
+		return 1;
+
+	monome->serial = serial;
+
+	/* request grid size here, etc */
+
+	return 0;
+}
 
 static void mext_free(monome_t *monome) {
 	SELF_FROM(monome);
@@ -38,7 +88,7 @@ monome_t *monome_protocol_new() {
 	if( !monome )
 		return NULL;
 
-	monome->open       = NULL;
+	monome->open       = mext_open;
 	monome->close      = NULL;
 	monome->free       = mext_free;
 
@@ -48,11 +98,10 @@ monome_t *monome_protocol_new() {
 	monome->intensity  = NULL;
 	monome->mode       = NULL;
 	
-	monome->led        = NULL;
+	monome->led        = mext_led;
 	monome->led_col    = NULL;
 	monome->led_row    = NULL;
 	monome->led_frame  = NULL;
-
 
 	return NULL;
 }
--- src/proto/mext.h
+++ src/proto/mext.h
@@ -14,7 +14,60 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+#include <stdint.h>
+
 #include <monome.h>
 #include "internal.h"
 
+#define PACKED __attribute__((packed))
+
 typedef monome_t mext_t;
+
+typedef enum {
+	SS_SYSTEM      = 0,
+	SS_LED_GRID    = 1,
+	SS_KEY_GRID    = 2,
+	SS_DIGITAL_OUT = 3,
+	SS_DIGITAL_IN  = 4,
+	SS_ENCODER     = 5,
+	SS_ANALOG_IN   = 6,
+	SS_ANALOG_OUT  = 7,
+	SS_TILT        = 8,
+	SS_LED_RING    = 9
+} mext_subsystem_t;
+
+typedef enum {
+	CMD_LED_ON        = 0,
+	CMD_LED_OFF       = 1,
+	CMD_LED_ALL_ON    = 2,
+	CMD_LED_ALL_OFF   = 3,
+	CMD_LED_FRAME     = 4,
+	CMD_LED_ROW       = 5,
+	CMD_LED_COLUMN    = 6,
+	CMD_LED_INTENSITY = 7
+} mext_cmd_t;
+
+typedef struct mext_msg mext_msg_t;
+
+struct PACKED mext_msg {
+	struct PACKED {
+		__extension__ mext_subsystem_t addr:4;
+		__extension__ mext_cmd_t cmd:4;
+	} header;
+
+	union PACKED {
+		struct PACKED {
+			uint8_t x;
+			uint8_t y;
+		} led;
+
+		struct PACKED {
+			struct PACKED {
+				uint8_t x;
+				uint8_t y;
+			} offset;
+
+			uint8_t data[8];
+		} frame;
+	} cmd;
+};
