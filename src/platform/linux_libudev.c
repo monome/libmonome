a5ae2a76a28541a56909c4904a5526b6b208b54e
William Light <wrl@illest.net>
2015-05-28 02:32:56 +0200

    linux_libudev: fix for recent libudev bug

--- src/platform/linux_libudev.c
+++ src/platform/linux_libudev.c
@@ -1,10 +1,10 @@
 /**
- * Copyright (c) 2010 William Light <wrl@illest.net>
- * 
+ * Copyright (c) 2015 William Light <wrl@illest.net>
+ *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
  * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
@@ -20,65 +20,47 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <sys/stat.h>
+
 #include <libudev.h>
 
 #include "platform.h"
 
-static char *get_monome_information(struct udev_device *d) {
-	const char *serial, *tty;
+static char *
+get_monome_information(struct udev_device *d)
+{
+	const char *serial;
 
-	assert(d);
+	serial = udev_device_get_property_value(d, "ID_SERIAL_SHORT");
 
-	if( !(tty    = udev_device_get_devnode(d)) ||
-		!(serial = udev_device_get_property_value(d, "ID_SERIAL_SHORT")) )
-		return NULL;
-
-	return strdup(serial);
+	return (serial) ? strdup(serial) : NULL;
 }
 
-static char *get_monome_information_from_syspath(struct udev *udev,
-                                                 const char *syspath) {
-	struct udev_device *d = NULL;
+char *
+monome_platform_get_dev_serial(const char *device)
+{
+	struct udev_device *dev;
+	struct stat statbuf;
+	struct udev *udev;
 	char *serial;
 
-	assert(syspath);
-	if( !(d = udev_device_new_from_syspath(udev, syspath)) )
-		return NULL;
-
-	serial = get_monome_information(d);
-	udev_device_unref(d);
-
-	return serial;
-}
-
-char *monome_platform_get_dev_serial(const char *device) {
-	struct udev *udev;
-	struct udev_enumerate *ue;
-	struct udev_list_entry *c;
-	const char *syspath;
-	char *serial = NULL;
-
-	assert(device);
+	if (stat(device, &statbuf) < 0 || !S_ISCHR(statbuf.st_mode))
+		goto err_stat;
 
 	udev = udev_new();
 
-	ue = udev_enumerate_new(udev);
-	udev_enumerate_add_match_property(ue, "DEVNAME", device);
+	if (!(dev = udev_device_new_from_devnum(udev, 'c', statbuf.st_rdev)))
+		goto err_no_device;
 
-	if( udev_enumerate_scan_devices(ue) )
-		goto err_scan;
+	serial = get_monome_information(dev);
 
-	c = udev_enumerate_get_list_entry(ue);
-
-	if( !(syspath = udev_list_entry_get_name(c)) )
-		goto err_nodevs; /* should NOT happen ever but whatever... */
-
-	serial = get_monome_information_from_syspath(udev, syspath);
-
-err_nodevs:
-	udev_enumerate_unref(ue);
-err_scan:
+	udev_device_unref(dev);
 	udev_unref(udev);
 
 	return serial;
+
+err_no_device:
+	udev_unref(udev);
+err_stat:
+	return NULL;
 }
